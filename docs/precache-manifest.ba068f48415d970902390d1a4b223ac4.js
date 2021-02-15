01443dddd9b827d3be56aafaea0c39425cb49c5b
mikezh <mikezhdev@gmail.com>
2021-02-15 13:17:29 +0800

    [update] gh-page

---
+++ docs/precache-manifest.ba068f48415d970902390d1a4b223ac4.js
@@ -0,0 +1,498 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34617e0e621527992f76ac75fb9cefc8",
+    "url": "/dashboard/img/scada/scada-guage.png"
+  },
+  {
+    "revision": "4cef02976541916b5821b1a688cfd0be",
+    "url": "/dashboard/img/scada/scada-label.png"
+  },
+  {
+    "revision": "6835d4fddfdebed18d32395dc192d951",
+    "url": "/dashboard/img/scada/scada-levelbar.png"
+  },
+  {
+    "revision": "473eb385c4408e862302d2e2538b29b3",
+    "url": "/dashboard/img/scada/scada-panel.png"
+  },
+  {
+    "revision": "ed509431a149d0362ac6aa47fcb3bae9",
+    "url": "/dashboard/img/scada/svg/btn_switch_001.svg"
+  },
+  {
+    "revision": "241754085c8b33af3ae324b093a0ddde",
+    "url": "/dashboard/img/scada/svg/btn_switch_001d.svg"
+  },
+  {
+    "revision": "f2413509849bb9acbeeb6f04e69519f2",
+    "url": "/dashboard/img/scada/svg/compressorA.svg"
+  },
+  {
+    "revision": "b61e999191ef1cf5a5d22cb044d775c2",
+    "url": "/dashboard/img/scada/svg/compressorA_pre.svg"
+  },
+  {
+    "revision": "81cda4275ef431b43e2aa5b62858c1e3",
+    "url": "/dashboard/img/scada/svg/ex-comp-1.svg"
+  },
+  {
+    "revision": "f968790b099e6e5dc10ae6806eb9fdf1",
+    "url": "/dashboard/img/scada/svg/ex-comp-1_.svg"
+  },
+  {
+    "revision": "5444448121867d4ffff20cfaeb27ee38",
+    "url": "/dashboard/img/scada/svg/ex-comp-2.svg"
+  },
+  {
+    "revision": "08f09b11c9ca83d5185965db6bfc335d",
+    "url": "/dashboard/img/scada/svg/ex-comp-2_pre.svg"
+  },
+  {
+    "revision": "1b45ab1804077566c48270fdfe25c14f",
+    "url": "/dashboard/img/scada/svg/ex-comp-3.svg"
+  },
+  {
+    "revision": "b576dc39ffd14c981c33f98944532093",
+    "url": "/dashboard/img/scada/svg/ex-comp-3_pre.svg"
+  },
+  {
+    "revision": "88c749eb9e3f8f9d1664034fc2a87a09",
+    "url": "/dashboard/img/scada/svg/ex1-ac0.svg"
+  },
+  {
+    "revision": "ef18c0be9ff9a07c6ea77814e6f0238d",
+    "url": "/dashboard/img/scada/svg/ex1-ac0_pre.svg"
+  },
+  {
+    "revision": "0f1c9f1fed9d817036a3f54a370d480a",
+    "url": "/dashboard/img/scada/svg/ex1-ac1.svg"
+  },
+  {
+    "revision": "89e26e507619429acb4a204664c0de43",
+    "url": "/dashboard/img/scada/svg/ex1-ac1_pre.svg"
+  },
+  {
+    "revision": "afed46cc08880e474f14c302c2544318",
+    "url": "/dashboard/img/scada/svg/ex1-ac2.svg"
+  },
+  {
+    "revision": "ff66abc55cd5c1e34685c25b5f5621df",
+    "url": "/dashboard/img/scada/svg/ex1-ac2_pre.svg"
+  },
+  {
+    "revision": "bf00b85bb8fb4577f518336898426bfd",
+    "url": "/dashboard/img/scada/svg/filter_hum_hot_001.svg"
+  },
+  {
+    "revision": "2223d42b4d0076ca13b1aa3b40248790",
+    "url": "/dashboard/img/scada/svg/filter_hum_hot_002.svg"
+  },
+  {
+    "revision": "6135639fe5115fdfb216856952d63110",
+    "url": "/dashboard/img/scada/svg/filter_hum_hot_003.svg"
+  },
+  {
+    "revision": "a78946c96bc0a255d2668b54c71e2724",
+    "url": "/dashboard/img/scada/svg/filter_hum_hot_004.svg"
+  },
+  {
+    "revision": "49b7e3b3a0a47610f74efc52a3806aad",
+    "url": "/dashboard/img/scada/svg/pumpA.svg"
+  },
+  {
+    "revision": "64146b1beb569350f7c6a1f7e5571afd",
+    "url": "/dashboard/img/scada/svg/pumpA_pre.svg"
+  },
+  {
+    "revision": "56aaae30a4460052500c279e08e2c55b",
+    "url": "/dashboard/img/scada/svg/rs-fengfenhunheqi.svg"
+  },
+  {
+    "revision": "4ec3802de2ee2fb270042290caec5ab5",
+    "url": "/dashboard/img/scada/svg/rs-fengfenhunheqi_pre.svg"
+  },
+  {
+    "revision": "a88636422d425d5a0c8cbe375c32c22f",
+    "url": "/dashboard/img/scada/svg/rs-fengji.svg"
+  },
+  {
+    "revision": "205fc19cd2479ca0b4f6f01e3b87d296",
+    "url": "/dashboard/img/scada/svg/rs-fengji_pre.svg"
+  },
+  {
+    "revision": "056b5068ad6b96ebaa6c105d14d1e497",
+    "url": "/dashboard/img/scada/svg/rs-fire.svg"
+  },
+  {
+    "revision": "9d0e0a22a12cb08da2e346fff3688113",
+    "url": "/dashboard/img/scada/svg/rs-fire_pre.svg"
+  },
+  {
+    "revision": "35966e7f00c84d6ea648e74532b85204",
+    "url": "/dashboard/img/scada/svg/rs-geifenqi.svg"
+  },
+  {
+    "revision": "dcb345947062b9971fc066336cdf491c",
+    "url": "/dashboard/img/scada/svg/rs-geifenqi_pre.svg"
+  },
+  {
+    "revision": "89f6a01cc2a4805085f4b0fe630413c3",
+    "url": "/dashboard/img/scada/svg/rs-guage.svg"
+  },
+  {
+    "revision": "4a8f4088e82ee6b516817806e26cbd6d",
+    "url": "/dashboard/img/scada/svg/rs-guage_pre.svg"
+  },
+  {
+    "revision": "91ccae762c886fa00535c705839a98af",
+    "url": "/dashboard/img/scada/svg/rs-guolu.svg"
+  },
+  {
+    "revision": "0a276b8222997f5f30b940be9170df61",
+    "url": "/dashboard/img/scada/svg/rs-guolu_pre.svg"
+  },
+  {
+    "revision": "9348487bbbd132761d3e5c6df46e1c66",
+    "url": "/dashboard/img/scada/svg/rs-handvalve.svg"
+  },
+  {
+    "revision": "f4846510f2d173d09bef415a02d8cb85",
+    "url": "/dashboard/img/scada/svg/rs-handvalve_pre.svg"
+  },
+  {
+    "revision": "2a28f97787438ed83fcabd0e4820ef73",
+    "url": "/dashboard/img/scada/svg/rs-meicang.svg"
+  },
+  {
+    "revision": "36f9a3b5a29685917e2c8be6cc7006ea",
+    "url": "/dashboard/img/scada/svg/rs-meicang_pre.svg"
+  },
+  {
+    "revision": "e62e852d5a3250b3c2efde309be80da4",
+    "url": "/dashboard/img/scada/svg/rs-tiaojiefa.svg"
+  },
+  {
+    "revision": "49ee504d2c9ce4f1aacb7844855359aa",
+    "url": "/dashboard/img/scada/svg/rs-tiaojiefa_pre.svg"
+  },
+  {
+    "revision": "3844a4cb2b0558b42c30112ab5636583",
+    "url": "/dashboard/img/scada/svg/rs-tongfengguan.svg"
+  },
+  {
+    "revision": "c989583fbfb6a16d7e611ddea8bc1c61",
+    "url": "/dashboard/img/scada/svg/rs-tongfengguan_pre.svg"
+  },
+  {
+    "revision": "e2ca8e99cb05aef50717dadcca97903d",
+    "url": "/dashboard/img/scada/svg/rs-tube.svg"
+  },
+  {
+    "revision": "80b5bd91b79323812e1594ff02e09093",
+    "url": "/dashboard/img/scada/svg/rs-tube_pre.svg"
+  },
+  {
+    "revision": "9f461119247dd0743ca80dc70899ed2a",
+    "url": "/dashboard/img/scada/svg/rs-valveA.svg"
+  },
+  {
+    "revision": "28518fac884b4631cb96831ef2f456a9",
+    "url": "/dashboard/img/scada/svg/rs-valveA_pre.svg"
+  },
+  {
+    "revision": "46bc38645af5d2e3b89f42e7d216fe5e",
+    "url": "/dashboard/img/scada/svg/rs-xichenqi.svg"
+  },
+  {
+    "revision": "ae00a22c1ae5f675ec5198fc42b6b32f",
+    "url": "/dashboard/img/scada/svg/rs-xichenqi_pre.svg"
+  },
+  {
+    "revision": "4802767cefbd1a0c172141b488c3673a",
+    "url": "/dashboard/img/scada/svg/rs-yalibiansong.svg"
+  },
+  {
+    "revision": "d2d6c2bf7143f85052cdaf1492a3cc48",
+    "url": "/dashboard/img/scada/svg/rs-yalibiansong_pre.svg"
+  },
+  {
+    "revision": "fe5c3c22a7414e33f7ff9273825f1962",
+    "url": "/dashboard/img/scada/svg/rs-youbeng.svg"
+  },
+  {
+    "revision": "529eef63d5958575810670d491cd6d7b",
+    "url": "/dashboard/img/scada/svg/rs-youbeng_pre.svg"
+  },
+  {
+    "revision": "8df6862bd6e226667243b40fc67c397c",
+    "url": "/dashboard/img/scada/svg/rs-youguan.svg"
+  },
+  {
+    "revision": "3cc0b2e7a3863a807bbda3200be688c1",
+    "url": "/dashboard/img/scada/svg/rs-youguan_pre.svg"
+  },
+  {
+    "revision": "5397c87000b71190e6dcd08e8e773cdf",
+    "url": "/dashboard/img/scada/svg/svg-sprite.801.svg"
+  },
+  {
+    "revision": "365bfcc7f04bad4e1a312ae0f440d862",
+    "url": "/dashboard/img/scada/svg/tube-h.svg"
+  },
+  {
+    "revision": "4559f1eaf63eb9195ebffb5cea5c3bea",
+    "url": "/dashboard/img/scada/svg/tube-l.svg"
+  },
+  {
+    "revision": "16bd45ccb067aad389788488430eb679",
+    "url": "/dashboard/img/scada/svg/tube-v.svg"
+  },
+  {
+    "revision": "e01b9093c8e5611518354dcbb4e790b2",
+    "url": "/dashboard/img/scada/svg/tube_h_pre.svg"
+  },
+  {
+    "revision": "56381bb3c7292dffb6b71e5f76aeb45f",
+    "url": "/dashboard/img/scada/svg/tube_v_pre.svg"
+  },
+  {
+    "revision": "2f74d515cd049953c4c69c5fa8a2f912",
+    "url": "/dashboard/img/scada/svg/valveA.svg"
+  },
+  {
+    "revision": "47327b210af5aa5d1577008b35e1ed1e",
+    "url": "/dashboard/img/scada/svg/valveA_pre.svg"
+  },
+  {
+    "revision": "3d6e7ff4c701286c5c6cf82115de357e",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "77ce7850ec52c00f10435500dac18052",
+    "url": "/dashboard/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dashboard/robots.txt"
+  },
+  {
+    "revision": "2d93552cbc4b4edd9815",
+    "url": "/dashboard/static/css/app.6d5c7357.css"
+  },
+  {
+    "revision": "ab8ff18ac9a21f50559b",
+    "url": "/dashboard/static/css/chunk-1e9bb7a6.9afa654b.css"
+  },
+  {
+    "revision": "85051c61cd60fa2b5a56",
+    "url": "/dashboard/static/css/chunk-635c93e8.a3d2b591.css"
+  },
+  {
+    "revision": "640032144f9bfd782b88",
+    "url": "/dashboard/static/css/chunk-6685ebe3.aed19415.css"
+  },
+  {
+    "revision": "c834b39e7a3607b0e239",
+    "url": "/dashboard/static/css/chunk-77bf1ee7.f9a1970c.css"
+  },
+  {
+    "revision": "f0372b15560855135b2a",
+    "url": "/dashboard/static/css/chunk-7acc864b.254f3e50.css"
+  },
+  {
+    "revision": "9162fe48b7d6b2c879e2",
+    "url": "/dashboard/static/css/chunk-9946fd82.5c51b666.css"
+  },
+  {
+    "revision": "65e771e66bc70cc11d8a",
+    "url": "/dashboard/static/css/chunk-b367f00e.b3106177.css"
+  },
+  {
+    "revision": "5f3829fe583d141096c7",
+    "url": "/dashboard/static/css/chunk-c40c38e4.53b1f4c3.css"
+  },
+  {
+    "revision": "3fff0bf60633ffc22f47",
+    "url": "/dashboard/static/css/chunk-c6a3b538.da4ec8f6.css"
+  },
+  {
+    "revision": "3c9ae8ff18b3f22850e3",
+    "url": "/dashboard/static/css/chunk-libs.8ea37153.css"
+  },
+  {
+    "revision": "054ea69d57c8bcec8d10e3b6aa9e414d",
+    "url": "/dashboard/static/fonts/MaterialIcons-Regular.054ea69d.ttf"
+  },
+  {
+    "revision": "0c90c0f933b81e0a0b961608c10d0eb9",
+    "url": "/dashboard/static/fonts/MaterialIcons-Regular.0c90c0f9.eot"
+  },
+  {
+    "revision": "731cfdfe786da7f3e927978333eb54b2",
+    "url": "/dashboard/static/fonts/MaterialIcons-Regular.731cfdfe.woff"
+  },
+  {
+    "revision": "a8c7cc2ebe47fe62aa8f350ebb736979",
+    "url": "/dashboard/static/fonts/MaterialIcons-Regular.a8c7cc2e.woff2"
+  },
+  {
+    "revision": "1b1710da6349426884d05129e16ed844",
+    "url": "/dashboard/static/fonts/MaterialIconsOutlined-Regular.1b1710da.woff"
+  },
+  {
+    "revision": "31a438db38050277dbeb1f29bae9f681",
+    "url": "/dashboard/static/fonts/MaterialIconsOutlined-Regular.31a438db.eot"
+  },
+  {
+    "revision": "7e988196bd1bfd8fe450dde5844ba8ab",
+    "url": "/dashboard/static/fonts/MaterialIconsOutlined-Regular.7e988196.woff2"
+  },
+  {
+    "revision": "8be31b5bfa6802b24e49f4cb88b54718",
+    "url": "/dashboard/static/fonts/MaterialIconsOutlined-Regular.8be31b5b.otf"
+  },
+  {
+    "revision": "06b9fb9fecbe6c2bb1b0bf2eeceda228",
+    "url": "/dashboard/static/fonts/MaterialIconsRound-Regular.06b9fb9f.eot"
+  },
+  {
+    "revision": "421badc32dd92c163c6d7c614fba685f",
+    "url": "/dashboard/static/fonts/MaterialIconsRound-Regular.421badc3.woff2"
+  },
+  {
+    "revision": "bf1d0521b4d4663c17a6bb7dc5e27390",
+    "url": "/dashboard/static/fonts/MaterialIconsRound-Regular.bf1d0521.woff"
+  },
+  {
+    "revision": "d283e90e36b77b49872a73f26463b8a9",
+    "url": "/dashboard/static/fonts/MaterialIconsRound-Regular.d283e90e.otf"
+  },
+  {
+    "revision": "1c579b12e59be6fa32a6c1f8760a27eb",
+    "url": "/dashboard/static/fonts/MaterialIconsSharp-Regular.1c579b12.eot"
+  },
+  {
+    "revision": "7adf9434a424a43fc96e1dcbdc0f7e1a",
+    "url": "/dashboard/static/fonts/MaterialIconsSharp-Regular.7adf9434.otf"
+  },
+  {
+    "revision": "c31894a7b9f7e7b001576f5c9b34ab37",
+    "url": "/dashboard/static/fonts/MaterialIconsSharp-Regular.c31894a7.woff"
+  },
+  {
+    "revision": "f5689b5e62dff43a48eeaf4c92c5da3d",
+    "url": "/dashboard/static/fonts/MaterialIconsSharp-Regular.f5689b5e.woff2"
+  },
+  {
+    "revision": "3fe5914acc97a2d1ba5171ed578bad38",
+    "url": "/dashboard/static/fonts/MaterialIconsTwoTone-Regular.3fe5914a.woff"
+  },
+  {
+    "revision": "9fd12b38058f5db9d35647b03af5cbc3",
+    "url": "/dashboard/static/fonts/MaterialIconsTwoTone-Regular.9fd12b38.eot"
+  },
+  {
+    "revision": "cf47e622d04aa476b511f1cac845487e",
+    "url": "/dashboard/static/fonts/MaterialIconsTwoTone-Regular.cf47e622.otf"
+  },
+  {
+    "revision": "edfde2d771d9612a07faf531997b74b1",
+    "url": "/dashboard/static/fonts/MaterialIconsTwoTone-Regular.edfde2d7.woff2"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/dashboard/static/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/dashboard/static/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dashboard/static/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dashboard/static/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dashboard/static/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dashboard/static/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dashboard/static/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "573255ced07e00d35d8d1eb071608132",
+    "url": "/dashboard/static/img/iconfont.573255ce.svg"
+  },
+  {
+    "revision": "2d93552cbc4b4edd9815",
+    "url": "/dashboard/static/js/app.4e3e8b7a.js"
+  },
+  {
+    "revision": "ab8ff18ac9a21f50559b",
+    "url": "/dashboard/static/js/chunk-1e9bb7a6.eef68d91.js"
+  },
+  {
+    "revision": "25dc9a57e2ad90ac2363",
+    "url": "/dashboard/static/js/chunk-2d0ded06.ceaf095e.js"
+  },
+  {
+    "revision": "85051c61cd60fa2b5a56",
+    "url": "/dashboard/static/js/chunk-635c93e8.8eae8cd0.js"
+  },
+  {
+    "revision": "640032144f9bfd782b88",
+    "url": "/dashboard/static/js/chunk-6685ebe3.0a045190.js"
+  },
+  {
+    "revision": "fee5449a08888f2fc3ce",
+    "url": "/dashboard/static/js/chunk-6b5a8300.386e93d8.js"
+  },
+  {
+    "revision": "c834b39e7a3607b0e239",
+    "url": "/dashboard/static/js/chunk-77bf1ee7.9effe5a3.js"
+  },
+  {
+    "revision": "f0372b15560855135b2a",
+    "url": "/dashboard/static/js/chunk-7acc864b.baa0aa93.js"
+  },
+  {
+    "revision": "9162fe48b7d6b2c879e2",
+    "url": "/dashboard/static/js/chunk-9946fd82.8bbd62c1.js"
+  },
+  {
+    "revision": "65e771e66bc70cc11d8a",
+    "url": "/dashboard/static/js/chunk-b367f00e.f5bb09a1.js"
+  },
+  {
+    "revision": "5f3829fe583d141096c7",
+    "url": "/dashboard/static/js/chunk-c40c38e4.f803828d.js"
+  },
+  {
+    "revision": "3fff0bf60633ffc22f47",
+    "url": "/dashboard/static/js/chunk-c6a3b538.51ffe22d.js"
+  },
+  {
+    "revision": "49349e2c32a59d030dff",
+    "url": "/dashboard/static/js/chunk-cdf61b6a.be47beb6.js"
+  },
+  {
+    "revision": "50117112c7716cfd43f5",
+    "url": "/dashboard/static/js/chunk-elementUI.ca10925f.js"
+  },
+  {
+    "revision": "3c9ae8ff18b3f22850e3",
+    "url": "/dashboard/static/js/chunk-libs.d1c431d9.js"
+  },
+  {
+    "revision": "5e3b3355cdc160dd9d62",
+    "url": "/dashboard/static/js/runtime.8b835cbf.js"
+  }
+]);
